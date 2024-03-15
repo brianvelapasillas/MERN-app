9669dd26da782e0c383257839dbc633d7c62b118
brianvelapasillas <velapasillasbrian@gmail.com>
2024-03-15 17:22:35 -0600

    added another post route

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ import path from "path";
 import { fileURLToPath } from "url";; //set the paths when we configure directories
 import authRoutes from "./routes/auth.js"; //paths and routes for every feature
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js"; //needed for line 40(register route)
+import {createPost} from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /*CONFIGURATIONS (functions that run in between diff things) */
 const __filename = fileURLToPath(import.meta.url);
@@ -42,9 +45,15 @@ const upload = multer({ storage });
 app.post("/auth/register", upload.single("picture"), register);    //if you wanna register, you will call this api from the frontend
                                                                    //this one cannot be on a separate file beause needs the upload right above
                                                                    //only when we need to upload a file, the other ones can be separate
+app.post("/posts", verifyToken, upload.single("picture"), createPost)  //allow user upload a picture, that's why this one is needed here
+                                                             //when we send from the front end the pic image, upload.single("picture") will grab it and upload from the http call into the local, "picture" can be named anything
+                                                             //createPost is another controller
+
+
 /* ROUTES */  //for our express router to use
-app.use("/auth, authRoutes");    
-app.use("/users", userRoutes);                                                               
+app.use("/auth", authRoutes);    
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);                                                              
 
 /* MONGOOSE SETUP*/
 const PORT = process.env.PORT || 6001; //port it goes if the other one doesn't work
---
+++ server/routes/posts.js
@@ -0,0 +1,37 @@
+import express from "express";
+import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* READ */
+router.get("/", verifyToken, getFeedPosts); //grab user feed when we are on the home page
+router.get("/:userId/posts", verifyToken, getFeedPosts); //grab only userID posts
+
+/* UPDATE */
+router.patch("/:id/like", verifyToken, likePost); //like and unlike post;
+
+export default router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
