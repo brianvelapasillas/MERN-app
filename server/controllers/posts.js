804b5c347c880875c365c11ca5f4747be4d4cdd9
brianvelapasillas <velapasillasbrian@gmail.com>
2024-03-16 10:54:50 -0600

    posts.js updated

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,13 +1,33 @@
 import Post from "../models/Post.js";
 
 /* CREATE */  
-export const createPost = async(re) //image getting passed through middleware, but not past it
+export const createPost = async(res) => { //image getting passed through middleware, but not past it
+    try {
+        const { userId, description, picturePath } = req.body; //front end is gonna send this 
+        const user = await User.findById(userId);
+        const newPost = new Post({
+            userId,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            location: user.location,
+            description,
+            userPicturePath: user.picturePath,
+            picturePath,
+            likes: {},     // this determines that someone liked it or not. If not listed didn't like it
+            comments: []     
+        })
+        await newPost.save();
+                                        //to save it, grab it(find), and return(post)
+        const post = await Post.find();    
+        res.status(201).json(post);
+    } catch (err) {
+        res.status(409).json({ message: err.message })
+    }
+}
 
 
 
 
-//front end is gonna send this 
-//
 
 
 
