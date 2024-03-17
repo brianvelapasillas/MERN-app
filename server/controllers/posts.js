a5705488051717be18f260d7b2d1d241019a1563
brianvelapasillas <velapasillasbrian@gmail.com>
2024-03-17 13:35:37 -0600

    fixing update

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -36,8 +36,41 @@ export const getFeedPosts = async (req, res) => {   //grab all the posts of ever
 }
 
 
-export const getUserPosts = async (req, res);
+export const getUserPosts = async (req, res) => {
+    try{
+        const { userId } = req.params;
+        const post = await Post.find({ userId });
+        res.satus(200).json(post);
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+}
 
+/* UPDATE*/
+export const likePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { userId } = req.body;
+        const post = await Post.findById(id);
+        const isLiked = post.likes.get(userId);
+
+        if (isLiked) {
+            post.likes.delete(userId);
+        } else {
+            post.likes.set(userId, true);
+        }
+
+        const updatePost = await Post.findByIdAndUpdate(
+            id,
+            { likes: post.likes },
+            { new: true }
+        );
+
+        res.status(200).json(updatePost);
+    } catch (err) {
+        res.satus(404).json({ message: err.message });
+    }
+}
 
 
 
